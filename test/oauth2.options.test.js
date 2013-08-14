373f79459769210bf4b42bc1db62ba6f77b6eadc
Jared Hanson <jaredhanson@gmail.com>
2013-08-14 12:26:47 -0700

    Implement proxy option.

--- test/oauth2.options.test.js
+++ test/oauth2.options.test.js
@@ -355,5 +355,84 @@ describe('OAuth2Strategy', function() {
     });
   });
   
+  describe('with relative callback URL and trust proxy option', function() {
+    var strategy = new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: '/auth/example/callback',
+        proxy: true
+      },
+      function(accessToken, refreshToken, profile, done) {
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+          return done(null, { id: '1234' }, { message: 'Hello' });
+        } else if (accessToken == '2YotnFZFEjr1zCsicMWpAA+INSECURE' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA+INSECURE') { 
+          return done(null, { id: '1234' }, { message: 'Hello (INSECURE)' });
+        }
+        return done(null, false);
+      });
+  
+    // inject a "mock" oauth2 instance
+    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+      if (options.grant_type !== 'authorization_code') { return callback(null, 'wrong-access-token', 'wrong-refresh-token'); }
+      
+      if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.redirect_uri == 'https://www.example.net/auth/example/callback') {
+        return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
+      } else if (code == 'SplxlOBeZQQYbYS6WxSbIA+INSECURE' && options.redirect_uri == 'http://www.example.net/auth/example/callback') {
+        return callback(null, '2YotnFZFEjr1zCsicMWpAA+INSECURE', 'tGzv3JOkF0XG5Qx2TlKWIA+INSECURE', { token_type: 'example', expires_in: 3600, example_parameter: 'example_value' });
+      } else {
+        return callback(null, 'wrong-access-token', 'wrong-refresh-token');
+      }
+    }
+    
+    describe('handling a request to be redirected for authorization that contains trusted x-forwarded-proto header', function() {
+      var url;
+
+      before(function(done) {
+        chai.passport(strategy)
+          .redirect(function(u) {
+            url = u;
+            done();
+          })
+          .req(function(req) {
+            req.url = '/auth/example';
+            req.headers.host = 'www.example.net';
+            req.headers['x-forwarded-proto'] = 'https';
+            req.connection = {};
+          })
+          .authenticate();
+      });
+
+      it('should be redirected', function() {
+        expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&client_id=ABC123&type=web_server');
+      });
+    });
+
+    describe('handling a request to be redirected for authorization that contains trusted x-forwarded-host header', function() {
+      var url;
+
+      before(function(done) {
+        chai.passport(strategy)
+          .redirect(function(u) {
+            url = u;
+            done();
+          })
+          .req(function(req) {
+            req.url = '/auth/example';
+            req.headers.host = 'server.internal';
+            req.headers['x-forwarded-proto'] = 'https';
+            req.headers['x-forwarded-host'] = 'www.example.net';
+            req.connection = {};
+          })
+          .authenticate();
+      });
+
+      it('should be redirected', function() {
+        expect(url).to.equal('https://www.example.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&client_id=ABC123&type=web_server');
+      });
+    });
+  });
+  
 });
 
