b65e2749cdd308ef2f30a3cdd00d897eb1761cc2
Jared Hanson <jaredhanson@gmail.com>
2013-08-14 17:38:43 -0700

    Increase test coverage.

--- test/oauth2.error.test.js
+++ test/oauth2.error.test.js
@@ -96,6 +96,51 @@ describe('OAuth2Strategy', function() {
     });
   });
   
+  describe('that encounters a node-oauth object literal error with nearly-OAuth-compatible body obtaining an access token', function() {
+    var strategy = new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.net/auth/example/callback',
+      },
+      function(accessToken, refreshToken, params, profile, done) {
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+          return done(null, { id: '1234' }, { message: 'Hello' });
+        }
+        return done(null, false);
+      });
+  
+    // inject a "mock" oauth2 instance
+    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+      return callback({ statusCode: 400, data: '{"error_code":"invalid_grant"}'});
+    }
+  
+    describe('handling an authorized return request', function() {
+      var err;
+  
+      before(function(done) {
+        chai.passport(strategy)
+          .error(function(e) {
+            err = e;
+            done();
+          })
+          .req(function(req) {
+            req.query = {};
+            req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
+          })
+          .authenticate();
+      });
+  
+      it('should error', function() {
+        expect(err).to.be.an.instanceof(InternalOAuthError)
+        expect(err.message).to.equal('Failed to obtain access token');
+        expect(err.oauthError.statusCode).to.equal(400);
+        expect(err.oauthError.data).to.equal('{"error_code":"invalid_grant"}');
+      });
+    });
+  });
+  
   describe('that encounters a node-oauth object literal error with non-OAuth-compatible body obtaining an access token', function() {
     var strategy = new OAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
