686703cd5a754ca9272d71ffb907a28385068b5d
Jared Hanson <jaredhanson@gmail.com>
2013-08-14 23:45:47 -0700

    Add test cases for redirecting with state.

--- test/oauth2.state.test.js
+++ test/oauth2.state.test.js
@@ -1,8 +1,87 @@
 var chai = require('chai')
+  , uri = require('url')
   , OAuth2Strategy = require('../lib/strategy');
 
 
 describe('OAuth2Strategy', function() {
+    
+  describe('with state checking enabled', function() {
+    var strategy = new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.net/auth/example/callback',
+        state: true
+      },
+      function(accessToken, refreshToken, profile, done) {
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+          return done(null, { id: '1234' }, { message: 'Hello' });
+        }
+        return done(null, false);
+      });
+  
+    // inject a "mock" oauth2 instance
+    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+      if (options.grant_type !== 'authorization_code') { return callback(null, 'wrong-access-token', 'wrong-refresh-token'); }
+      
+      if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.redirect_uri == 'https://www.example.net/auth/example/callback') {
+        callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
+      } else {
+        callback(null, 'wrong-access-token', 'wrong-refresh-token');
+      }
+    }
+  
+    describe('handling a request to be redirected for authorization', function() {
+      var request, url;
+  
+      before(function(done) {
+        chai.passport(strategy)
+          .redirect(function(u) {
+            url = u;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+            req.session = {};
+          })
+          .authenticate();
+      });
+  
+      it('should be redirected', function() {
+        var u = uri.parse(url, true);
+        expect(u.query.state).to.have.length(24);
+      });
+      
+      it('should save state in session', function() {
+        var u = uri.parse(url, true);
+        
+        expect(request.session['oauth2:www.example.com'].state).to.have.length(24);
+        expect(request.session['oauth2:www.example.com'].state).to.equal(u.query.state);
+      });
+    });
+    
+    describe('handling a request without session to be redirected for authorization', function() {
+      var request, url;
+  
+      before(function(done) {
+        chai.passport(strategy)
+          .error(function(e) {
+            err = e;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+          })
+          .authenticate();
+      });
+  
+      it('should error', function() {
+        expect(err).to.be.an.instanceof(Error)
+        expect(err.message).to.equal('OAuth2Strategy requires session support when using state. Did you forget app.use(express.session(...))?');
+      });
+    });
+  });
     
   describe('with explicit state declared as authenticate option', function() {
     var strategy = new OAuth2Strategy({
