4fe704f063bc6b0fb6aa99e86eda36bff27743b0
Jared Hanson <jaredhanson@gmail.com>
2013-08-15 08:28:05 -0700

    Test case for sessionKey option.

--- test/oauth2.state.test.js
+++ test/oauth2.state.test.js
@@ -5,8 +5,6 @@ var chai = require('chai')
 
 describe('OAuth2Strategy', function() {
     
-  // WIP: Add test cases for state and sessionKey options
-    
   describe('with state checking enabled', function() {
     var strategy = new OAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -250,6 +248,104 @@ describe('OAuth2Strategy', function() {
       it('should error', function() {
         expect(err).to.be.an.instanceof(Error)
         expect(err.message).to.equal('OAuth2Strategy requires session support when using state. Did you forget app.use(express.session(...))?');
+      });
+    });
+  });
+  
+  describe('with state checking enabled and session key option', function() {
+    var strategy = new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.net/auth/example/callback',
+        state: true,
+        sessionKey: 'oauth2:example'
+      },
+      function(accessToken, refreshToken, profile, done) {
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+          return done(null, { id: '1234' }, { message: 'Hello' });
+        }
+        return done(null, false);
+      });
+  
+    // inject a "mock" oauth2 instance
+    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+      if (options.grant_type !== 'authorization_code') { return callback(null, 'wrong-access-token', 'wrong-refresh-token'); }
+      
+      if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.redirect_uri == 'https://www.example.net/auth/example/callback') {
+        callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
+      } else {
+        callback(null, 'wrong-access-token', 'wrong-refresh-token');
+      }
+    }
+    
+    describe('handling an authorized return request with correct state', function() {
+      var request
+        , user
+        , info;
+  
+      before(function(done) {
+        chai.passport(strategy)
+          .success(function(u, i) {
+            user = u;
+            info = i;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+            
+            req.query = {};
+            req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
+            req.query.state = 'DkbychwKu8kBaJoLE5yeR5NK';
+            req.session = {};
+            req.session['oauth2:example'] = {};
+            req.session['oauth2:example']['state'] = 'DkbychwKu8kBaJoLE5yeR5NK';
+          })
+          .authenticate();
+      });
+  
+      it('should supply user', function() {
+        expect(user).to.be.an.object;
+        expect(user.id).to.equal('1234');
+      });
+  
+      it('should supply info', function() {
+        expect(info).to.be.an.object;
+        expect(info.message).to.equal('Hello');
+      });
+      
+      it('should remove state from session', function() {
+        expect(request.session['oauth2:example']).to.be.undefined;
+      });
+    });
+  
+    describe('handling a request to be redirected for authorization', function() {
+      var request, url;
+  
+      before(function(done) {
+        chai.passport(strategy)
+          .redirect(function(u) {
+            url = u;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+            req.session = {};
+          })
+          .authenticate();
+      });
+  
+      it('should be redirected', function() {
+        var u = uri.parse(url, true);
+        expect(u.query.state).to.have.length(24);
+      });
+      
+      it('should save state in session', function() {
+        var u = uri.parse(url, true);
+        
+        expect(request.session['oauth2:example'].state).to.have.length(24);
+        expect(request.session['oauth2:example'].state).to.equal(u.query.state);
       });
     });
   });
