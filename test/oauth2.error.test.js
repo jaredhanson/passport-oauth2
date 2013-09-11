55eea526c84c9178b739e8f48c3c6707fd3e49e1
Jared Hanson <jaredhanson@gmail.com>
2013-09-11 08:33:39 -0700

    Invoke verify callback inside try/catch.

--- test/oauth2.error.test.js
+++ test/oauth2.error.test.js
@@ -231,4 +231,49 @@ describe('OAuth2Strategy', function() {
     });
   });
   
+  describe('that encounters a thrown error during verification', function() {
+    
+    var strategy = new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.net/auth/example/callback',
+      },
+      function(accessToken, refreshToken, params, profile, done) {
+        throw new Error('something was thrown');
+      });
+  
+    // inject a "mock" oauth2 instance
+    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+      if (code == 'SplxlOBeZQQYbYS6WxSbIA' && options.grant_type == 'authorization_code') {
+        return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
+      } else {
+        return callback(null, 'wrong-access-token', 'wrong-refresh-token');
+      }
+    }
+  
+    describe('handling an authorized return request', function() {
+      var err;
+  
+      before(function(done) {
+        chai.passport(strategy)
+          .error(function(e) {
+            err = e;
+            done();
+          })
+          .req(function(req) {
+            req.query = {};
+            req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
+          })
+          .authenticate();
+      });
+  
+      it('should error', function() {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.equal('something was thrown');
+      });
+    });
+  });
+  
 });
