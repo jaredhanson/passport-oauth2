a3db2b40628359a36b12d5794284b768c9252c77
Jared Hanson <jaredhanson@gmail.com>
2015-10-23 10:40:28 -0700

    Test case for S256 PKCE method.

--- test/oauth2.pkce.test.js
+++ test/oauth2.pkce.test.js
@@ -69,4 +69,69 @@ describe('OAuth2Strategy', function() {
     });
   });
   
+  describe('with PKCE S256 transformation method', function() {
+    var mockCrypto = {
+      pseudoRandomBytes: function(len) {
+        if (len !== 32) { throw new Error('xyz'); }
+        // https://tools.ietf.org/html/rfc7636#appendix-B
+        return new Buffer(
+          [116, 24, 223, 180, 151, 153, 224, 37, 79, 250, 96, 125, 216, 173,
+          187, 186, 22, 212, 37, 77, 105, 214, 191, 240, 91, 88, 5, 88, 83,
+          132, 141, 121]
+        );
+      }
+    }
+    
+    var OAuth2Strategy = require('proxyquire')('../lib/strategy', { crypto: mockCrypto });
+    var strategy = new OAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.net/auth/example/callback',
+        state: true,
+        pkceMethod: 'S256'
+      },
+      function(accessToken, refreshToken, profile, done) {
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+          return done(null, { id: '1234' }, { message: 'Hello' });
+        }
+        return done(null, false);
+      });
+      
+      
+    describe('handling a request to be redirected for authorization', function() {
+      var request, url;
+
+      before(function(done) {
+        chai.passport.use(strategy)
+          .redirect(function(u) {
+            url = u;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+            req.session = {};
+          })
+          .authenticate();
+      });
+
+      it('should be redirected', function() {
+        var u = uri.parse(url, true);
+        expect(u.query.state).to.have.length(24);
+        expect(u.query.code_challenge).to.have.length(43);
+        expect(u.query.code_challenge).to.equal('E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM')
+        expect(u.query.code_challenge_method).to.equal('S256');
+      });
+    
+      it('should save state in session', function() {
+        var u = uri.parse(url, true);
+        expect(request.session['oauth2:www.example.com'].state).to.have.length(24);
+        expect(request.session['oauth2:www.example.com'].state).to.equal(u.query.state);
+        expect(request.session['oauth2:www.example.com'].verifier).to.have.length(43);
+        expect(request.session['oauth2:www.example.com'].verifier).to.equal('dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk');
+      });
+    });
+  });
+  
 });
