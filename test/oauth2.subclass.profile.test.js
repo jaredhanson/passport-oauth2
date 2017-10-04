57d371195974bddb7eb3c166c375c6525f4400e6
German Lena <german.lena@gmail.com>
2017-10-04 16:11:52 -0300

    Propagate token exchange params to load user function

--- test/oauth2.subclass.profile.test.js
+++ test/oauth2.subclass.profile.test.js
@@ -4,7 +4,7 @@ var OAuth2Strategy = require('../lib/strategy')
 
 
 describe('OAuth2Strategy subclass', function() {
-  
+
   describe('that overrides userProfile', function() {
     function FooOAuth2Strategy(options, verify) {
       OAuth2Strategy.call(this, options, verify);
@@ -13,13 +13,13 @@ describe('OAuth2Strategy subclass', function() {
 
     FooOAuth2Strategy.prototype.userProfile = function(accessToken, done) {
       if (accessToken === '666') { return done(new Error('something went wrong loading user profile')); }
-      
+
       if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
-      
+
       return done(null, { username: 'jaredhanson', location: 'Oakland, CA' });
     }
-    
-    
+
+
     describe('fetching user profile', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -32,19 +32,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile.username != 'jaredhanson') { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234', username: profile.username }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var user
         , info;
 
@@ -73,7 +73,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // fetching user profile
-    
+
     describe('error fetching user profile', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -85,16 +85,16 @@ describe('OAuth2Strategy subclass', function() {
       function(accessToken, refreshToken, profile, done) {
         return done(new Error('verify callback should not be called'));
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '666', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var err;
 
       before(function(done) {
@@ -115,7 +115,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(err.message).to.equal('something went wrong loading user profile');
       });
     }); // error fetching user profile
-    
+
     describe('skipping user profile due to skipUserProfile option set to true', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -129,19 +129,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile !== undefined) { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234' }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var user
         , info;
 
@@ -169,7 +169,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // skipping user profile due to skipUserProfile option set to true
-    
+
     describe('not skipping user profile due to skipUserProfile returning false', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -185,19 +185,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile.username != 'jaredhanson') { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234', username: profile.username }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var user
         , info;
 
@@ -226,7 +226,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // not skipping user profile due to skipUserProfile returning false
-    
+
     describe('skipping user profile due to skipUserProfile returning true', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -242,19 +242,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile !== undefined) { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234' }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var user
         , info;
 
@@ -282,7 +282,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // skipping user profile due to skipUserProfile returning true
-    
+
     describe('not skipping user profile due to skipUserProfile asynchronously returning false', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -292,7 +292,7 @@ describe('OAuth2Strategy subclass', function() {
         callbackURL: 'https://www.example.net/auth/example/callback',
         skipUserProfile: function(accessToken, done) {
           if (accessToken != '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect token argument')); }
-          
+
           return done(null, false);
         }
       },
@@ -300,19 +300,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile.username != 'jaredhanson') { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234', username: profile.username }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var user
         , info;
 
@@ -341,7 +341,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // not skipping user profile due to skipUserProfile asynchronously returning false
-    
+
     describe('skipping user profile due to skipUserProfile asynchronously returning true', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -351,7 +351,7 @@ describe('OAuth2Strategy subclass', function() {
         callbackURL: 'https://www.example.net/auth/example/callback',
         skipUserProfile: function(accessToken, done) {
           if (accessToken != '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect token argument')); }
-          
+
           return done(null, true);
         }
       },
@@ -359,19 +359,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile !== undefined) { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234' }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var user
         , info;
 
@@ -399,7 +399,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // skipping user profile due to skipUserProfile asynchronously returning true
-    
+
     describe('error due to skipUserProfile asynchronously returning error', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -415,19 +415,19 @@ describe('OAuth2Strategy subclass', function() {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
         if (profile !== undefined) { return done(new Error('incorrect profile argument')); }
-        
+
         return done(null, { id: '1234' }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
-        
+
         return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-  
-  
+
+
       var request
         , err;
 
@@ -449,7 +449,80 @@ describe('OAuth2Strategy subclass', function() {
         expect(err.message).to.equal('something went wrong');
       });
     }); // error due to skipUserProfile asynchronously returning error
-    
+
   }); // that overrides userProfile
-  
+
+  describe('that overrides userProfile (with params)', function() {
+    function FooOAuth2Strategy(options, verify) {
+      OAuth2Strategy.call(this, options, verify);
+    }
+    util.inherits(FooOAuth2Strategy, OAuth2Strategy);
+
+    FooOAuth2Strategy.prototype.userProfile = function(accessToken, params, done) {
+      if (accessToken === '666') { return done(new Error('something went wrong loading user profile')); }
+
+      if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
+
+      if (params.token_type !== 'example') { return done(new Error('incorrect params argument')); }
+
+      if (typeof(done) !== 'function') { return done(new Error('incorrect done argument')); }
+
+      return done(null, { username: 'jaredhanson', location: 'Oakland, CA' });
+    }
+
+    describe('fetching user profile', function() {
+      var strategy = new FooOAuth2Strategy({
+        authorizationURL: 'https://www.example.com/oauth2/authorize',
+        tokenURL: 'https://www.example.com/oauth2/token',
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.net/auth/example/callback',
+      },
+      function(accessToken, refreshToken, profile, done) {
+        if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
+        if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
+        if (profile.username != 'jaredhanson') { return done(new Error('incorrect profile argument')); }
+
+        return done(null, { id: '1234', username: profile.username }, { message: 'Hello' });
+      });
+
+      strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+        if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
+        if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
+        if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
+
+        return callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
+      }
+
+
+      var user
+        , info;
+
+      before(function(done) {
+        chai.passport.use(strategy)
+          .success(function(u, i) {
+            user = u;
+            info = i;
+            done();
+          })
+          .req(function(req) {
+            req.query = {};
+            req.query.code = 'SplxlOBeZQQYbYS6WxSbIA';
+          })
+          .authenticate();
+      });
+
+      it('should supply user', function() {
+        expect(user).to.be.an.object;
+        expect(user.id).to.equal('1234');
+        expect(user.username).to.equal('jaredhanson');
+      });
+
+      it('should supply info', function() {
+        expect(info).to.be.an.object;
+        expect(info.message).to.equal('Hello');
+      });
+    }); // fetching user profile
+  });
+
 });
