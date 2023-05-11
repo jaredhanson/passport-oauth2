1b747b51194990fb03d8839461257b2eb2ced23f
Jonathon Storer <jonathon.scott.storer@gmail.com>
2023-05-11 10:33:12 -0400

    Add handler for subclasses to process the response from OAuth2 token request before validation

--- test/oauth2.subclass.test.js
+++ test/oauth2.subclass.test.js
@@ -4,9 +4,8 @@ var OAuth2Strategy = require('../lib/strategy')
   , chai = require('chai')
   , util = require('util');
 
-
 describe('OAuth2Strategy subclass', function() {
-  
+
   describe('that overrides authorizationParams', function() {
     function FooOAuth2Strategy(options, verify) {
       OAuth2Strategy.call(this, options, verify);
@@ -16,8 +15,8 @@ describe('OAuth2Strategy subclass', function() {
     FooOAuth2Strategy.prototype.authorizationParams = function(options) {
       return { prompt: options.prompt };
     }
-    
-    
+
+
     describe('issuing authorization request that redirects to service provider', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -29,14 +28,14 @@ describe('OAuth2Strategy subclass', function() {
       function(accessToken, refreshToken, profile, done) {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
-        
+
         return done(null, { id: '1234' }, { message: 'Hello' });
       });
-    
-  
+
+
       describe('with prompt', function() {
         var url;
-  
+
         before(function(done) {
           chai.passport.use(strategy)
             .redirect(function(u) {
@@ -47,15 +46,15 @@ describe('OAuth2Strategy subclass', function() {
             })
             .authenticate({ prompt: 'mobile' });
         });
-  
+
         it('should be redirected', function() {
           expect(url).to.equal('https://www.example.com/oauth2/authorize?prompt=mobile&response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&client_id=ABC123');
         });
       }); // with prompt
-    
+
       describe('with scope and prompt', function() {
         var url;
-  
+
         before(function(done) {
           chai.passport.use(strategy)
             .redirect(function(u) {
@@ -66,17 +65,17 @@ describe('OAuth2Strategy subclass', function() {
             })
             .authenticate({ scope: 'email', prompt: 'mobile' });
         });
-  
+
         it('should be redirected', function() {
           expect(url).to.equal('https://www.example.com/oauth2/authorize?prompt=mobile&response_type=code&redirect_uri=https%3A%2F%2Fwww.example.net%2Fauth%2Fexample%2Fcallback&scope=email&client_id=ABC123');
         });
       }); // with scope and prompt
-    
+
     }); // issuing authorization request that redirects to service provider
-    
+
   }); // that overrides authorizationParams
-  
-  
+
+
   describe('that overrides tokenParams', function() {
     function FooOAuth2Strategy(options, verify) {
       OAuth2Strategy.call(this, options, verify);
@@ -86,8 +85,8 @@ describe('OAuth2Strategy subclass', function() {
     FooOAuth2Strategy.prototype.tokenParams = function(options) {
       return { type: options.type };
     }
-    
-    
+
+
     describe('processing response to authorization request that was approved', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -99,23 +98,23 @@ describe('OAuth2Strategy subclass', function() {
       function(accessToken, refreshToken, profile, done) {
         if (accessToken !== '2YotnFZFEjr1zCsicMWpAA') { return done(new Error('incorrect accessToken argument')); }
         if (refreshToken !== 'tGzv3JOkF0XG5Qx2TlKWIA') { return done(new Error('incorrect refreshToken argument')); }
-        
+
         return done(null, { id: '1234' }, { message: 'Hello' });
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         if (code !== 'SplxlOBeZQQYbYS6WxSbIA') { return callback(new Error('incorrect code argument')); }
         if (options.grant_type !== 'authorization_code') { return callback(new Error('incorrect options.grant_type argument')); }
         if (options.redirect_uri !== 'https://www.example.net/auth/example/callback') { return callback(new Error('incorrect options.redirect_uri argument')); }
         if (options.type !== 'web_server') { return callback(new Error('incorrect options.type argument')); }
-        
+
         callback(null, '2YotnFZFEjr1zCsicMWpAA', 'tGzv3JOkF0XG5Qx2TlKWIA', { token_type: 'example' });
       }
-      
-      
+
+
       var user
         , info;
-  
+
       before(function(done) {
         chai.passport.use(strategy)
           .success(function(u, i) {
@@ -140,10 +139,10 @@ describe('OAuth2Strategy subclass', function() {
         expect(info.message).to.equal('Hello');
       });
     }); // processing response to authorization request that was approved
-    
+
   }); // that overrides tokenParams
-  
-  
+
+
   describe('that overrides parseErrorResponse', function() {
     function FooOAuth2Strategy(options, verify) {
       OAuth2Strategy.call(this, options, verify);
@@ -152,14 +151,14 @@ describe('OAuth2Strategy subclass', function() {
 
     FooOAuth2Strategy.prototype.parseErrorResponse = function(body, status) {
       if (status === 500) { throw new Error('something went horribly wrong'); }
-  
+
       var e = new Error('Custom OAuth error');
       e.body = body;
       e.status = status;
       return e;
     }
-    
-    
+
+
     describe('and supplies error', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -169,17 +168,17 @@ describe('OAuth2Strategy subclass', function() {
         callbackURL: 'https://www.example.net/auth/example/callback',
       },
       function(accessToken, refreshToken, profile, done) {
-        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') {
           return done(null, { id: '1234' }, { message: 'Hello' });
         }
         return done(null, false);
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         return callback({ statusCode: 400, data: 'Invalid code' });
       }
-    
-    
+
+
       var err;
 
       before(function(done) {
@@ -202,7 +201,7 @@ describe('OAuth2Strategy subclass', function() {
         expect(err.status).to.equal(400);
       });
     }); // and supplies error
-    
+
     describe('and throws exception', function() {
       var strategy = new FooOAuth2Strategy({
         authorizationURL: 'https://www.example.com/oauth2/authorize',
@@ -212,17 +211,17 @@ describe('OAuth2Strategy subclass', function() {
         callbackURL: 'https://www.example.net/auth/example/callback',
       },
       function(accessToken, refreshToken, profile, done) {
-        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') { 
+        if (accessToken == '2YotnFZFEjr1zCsicMWpAA' && refreshToken == 'tGzv3JOkF0XG5Qx2TlKWIA') {
           return done(null, { id: '1234' }, { message: 'Hello' });
         }
         return done(null, false);
       });
-      
+
       strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
         return callback({ statusCode: 500, data: 'Invalid code' });
       }
-    
-    
+
+
       var err;
 
       before(function(done) {
@@ -245,7 +244,154 @@ describe('OAuth2Strategy subclass', function() {
         expect(err.oauthError.data).to.equal('Invalid code');
       });
     }); // and throws exception
-    
+
   }); // that overrides parseErrorResponse
-  
+
+  describe('that overrides handleOAuthAccessTokenReponse', function () {
+    var options = {
+      authorizationURL: 'https://www.example.com/oauth2/authorize',
+      tokenURL: 'https://www.example.com/oauth2/token',
+      clientID: 'clientId',
+      clientSecret: 'clientSecret',
+      callbackURL: 'https://www.example.net/auth/example/callback',
+    };
+
+    function BarOAuth2Strategy(options, verify) {
+      OAuth2Strategy.call(this, options, verify);
+    }
+    util.inherits(BarOAuth2Strategy, OAuth2Strategy);
+
+    BarOAuth2Strategy.prototype.handleOAuthAccessTokenReponse = function(accessToken, refreshToken, params, done) {
+      done(new Error('this needs to be defined in each test'));
+    }
+
+    var strategy = new BarOAuth2Strategy(options, function(accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    });
+
+    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+      return callback(null, 'access-token', 'refresh-token', { token_type: 'example' });
+    }
+
+    beforeEach(function () {
+      strategy.__verify = strategy._verify;
+    });
+
+    afterEach(function () {
+      strategy._verify = strategy.__verify;
+      delete strategy.__verify;
+    });
+
+    describe('failure', function () {
+      var err;
+
+      describe('handleOAuthAccessTokenReponse ecounters an exception', function () {
+        beforeEach(function(done) {
+          strategy._verify = function (at, rt, params, done) {
+            return done(new Error('verify callback should not be called'));
+          }
+
+          strategy.__proto__.handleOAuthAccessTokenReponse = function(at, rt, p, done) { done(new Error()); }
+
+          chai.passport.use(strategy)
+            .error(function(e) {
+              err = e;
+              done();
+            })
+            .req(function(req) {
+              req.query = {};
+              req.query.code = 'authorization-code';
+            })
+            .authenticate();
+        });
+
+        it('should error', function() {
+          expect(err).to.be.an.instanceof(Error)
+          expect(err.message).to.equal('Failed to handle oauth access token response');
+        });
+      }); // exception encountered in handleOAuthAccessTokenReponse
+
+      describe('handleOAuthAccessTokenReponse does not pass an accessToken back', function () {
+        beforeEach(function(done) {
+          strategy.__proto__.handleOAuthAccessTokenReponse = function(at, rt, p, done) {
+            done();
+          }
+
+          chai.passport.use(strategy)
+            .error(function(e) {
+              err = e;
+              done();
+            })
+            .req(function(req) {
+              req.query = {};
+              req.query.code = 'authorization-code';
+            })
+            .authenticate();
+        });
+
+        it('should error', function() {
+          expect(err).to.be.an.instanceof(Error)
+          expect(err.message).to.equal('Failed to obtain access token');
+        });
+      }); // failed to pass back accessToken handleOAuthAccessTokenReponse
+    });
+
+    describe('success', function () {
+      var user
+        , info
+        , accessToken;
+
+      beforeEach(function() {
+        strategy._verify = function (accessToken, refreshToken, params, profile, next) {
+          profile.user = true;
+          profile.accessToken = accessToken;
+          profile.refreshToken = refreshToken;
+
+          next(null, profile, params)
+        }
+
+        strategy.__proto__.handleOAuthAccessTokenReponse = function(at, rt, p, done) {
+          expect(at).to.eql('at');
+          expect(rt).to.eql('rt');
+          expect(p).to.eql({ token_type: 'test' });
+
+          process.nextTick(function () {
+            at = 'newAt';
+            rt = 'newRt';
+            p.token_type = 'new token type';
+
+            done(null, at, rt, p);
+          });
+        }
+
+        strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
+          return callback(null, 'at', 'rt', { token_type: 'test' });
+        }
+      });
+
+      it('alters the accessToken, refreshToken, and params', function(done) {
+        chai.passport.use(strategy)
+          .success(function(profile, info) {
+            try {
+              expect(profile).to.be.an.object;
+              expect(profile.user).to.be.true;
+              expect(profile.accessToken).to.eql('newAt');
+              expect(profile.refreshToken).to.eql('newRt');
+
+              expect(info).to.be.an.object;
+              expect(info.token_type).to.eql('new token type');
+            } catch (e) {
+              return done(e);
+            }
+            return done();
+          })
+          .req(function(req) {
+            req.query = { code: 'code' };
+          })
+          .authenticate();
+      }); // failed to pass back accessToken handleOAuthAccessTokenReponse
+    });
+
+  }); // that overrides handleOAuthAccessTokenReponse
+
 });
